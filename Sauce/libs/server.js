5c9803827de599d897fad322ccbd238a53018720
Agne Ødegaard <agne.odegaard@adresseavisen.no>
2022-06-26 16:56:47 +0200

    mime types

--- Sauce/libs/server.js
+++ Sauce/libs/server.js
@@ -1,33 +1,41 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const http = require('http');
 
 const path = require('path');
 const color = require('./color');
+const mime = require('mime-types');
 
 
 module.exports = server = ({
-    port = 1337,
-    logger = false
+    port = 1337
 } = {}) => () => new Promise((resolve, reject) => {
 
     // Create a local server to receive data from
     const server = http.createServer((req, res) => {
         const url = req.url;
-        const error = (err, req, res) => {
+        const error = (err, _, res) => {
             res.writeHead(404, { 'Content-Type': 'text/html' });
             res.end(require('./templates/error')(err));
         }
 
+        const sendFile = (file, _, res) => fs.access(file)
+            .then(() => fs.readFile(file), err => error(err, _, res))
+            .then(contents => {
+                res.writeHead(200, { 'Content-Type': mime.lookup(path.extname(file)) || 'text/plain' });
+                res.end(contents);
+            }, err => error(err, _, res));
+
         const pages = {
-            '/': (req, res) => {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                if (fs.existsSync('dist/_error.html')) return res.end(fs.readFileSync('dist/_error.html'))
-                return res.end(fs.readFileSync('dist/index.html'));
+            '/': (...args) => {
+                const errorFile = path.join(process.cwd(), 'dist', '_error.html');
+                const indexFile = path.join(process.cwd(), 'dist', 'index.html');
+
+                fs.access(errorFile)
+                    .then(() => sendFile(errorFile, ...args)
+                        .catch(err => error(err, ...args)))
+                    .catch(() => sendFile(indexFile, ...args))
             },
-            '*': (req, res) => {
-                const file = path.join(process.cwd(), 'dist', url);
-                fs.promises.access(file).then(() => res.end(fs.readFileSync(file)), err => error(err, req, res));
-            }
+            '*': (...args) => sendFile(path.join(process.cwd(), 'dist', url), ...args)
         }
 
         const callback = pages[url] || pages['*'] || ((...args) => error(404, ...args));
@@ -38,6 +46,7 @@ module.exports = server = ({
 
     const info = server.address();
     info.address = info.address == '::' ? 'localhost' : info.address;
+
     resolve({
         out: color.cyan('server: ') + `http://${info.address}:${info.port}`
     });
